044ddc021ab6254da7e31d404caead1e59da9e3e
Fede <107002434+Splix777@users.noreply.github.com>
2023-06-01 16:18:17 +0200

    Add files via upload

---
+++ philo/philosophers.h
@@ -0,0 +1,46 @@
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+/*memset*/
+#include <string.h>
+/*printf*/
+#include <stdio.h>
+/*malloc, free*/
+#include <stdlib.h>
+/*write, usleep*/
+#include <unistd.h>
+/*gettimeofday*/
+#include <sys/time.h>
+/*pthread_creat, pthread_detach, pthread_join, pthread_mutex_init, pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock*/
+#include <pthread.h>
+
+# define DEAD 0
+# define ALIVE 1
+
+typedef struct s_sim
+{
+    int n_philo;
+    int n_forks;
+    int argc;
+    char **argv;
+}               t_sim;
+
+typedef struct s_philo
+{
+    int n_meals;
+    int t_die;
+    int t_eat;
+    int t_sleep;
+    int left_fork;
+    int right_fork;
+}               t_philo;
+
+// utils.c
+int     ft_atoi(char *str);
+int     is_num(char *str);
+// errors_and_exit.c
+void    exit_error(char *str);
+// main.c
+
+
+
+#endif
---
+++ philo/utils.c
@@ -0,0 +1,40 @@
+#include "philosophers.h"
+
+int ft_atoi(char *str)
+{
+    int nbr;
+    int sign;
+    int i;
+
+    i = 0;
+    sign = 1;
+    nbr = 0;
+    while (str[i] != 32 && (str[i] >= 9 && str[i] <= 13))
+        i++;
+    if (str[i] == '-' || str[i] == '+')
+    {
+        if (str[i] == '-')
+            sign *= -1;
+        i++;
+    }
+    while (str[i] >= '0' && str[i] <= '9')
+    {
+        nbr = nbr * 10 + (str[i] - '0');
+        i++;
+    }
+    return (nbr * sign);
+}
+
+int is_num(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i])
+    {
+        if (str[i] < '0' || str[i] > '9')
+            return (0);
+        i++;
+    }
+    return (1);
+}
